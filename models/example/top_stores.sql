4274ea56b7b8e4b5636ec73b4bc54d9b6a0bc567
BenRodgers <benjaminarodgers@gmail.com>
2022-07-24 01:05:47 +1000

    Added new model

---
+++ models/example/top_stores.sql
@@ -0,0 +1,15 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+SELECT Store_ID_, Store_Sales
+FROM {{ source('raw_layer', 'stores') }}
+ORDER BY 2 DESC
+
